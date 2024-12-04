a2564487e35a0141a491d8fe4ba9363d15e7b0c6
Luke <venterluke14@gmail.com>
2024-12-04 19:49:44 +0200

    comit

--- CSVReader.h
+++ CSVReader.h
@@ -9,7 +9,7 @@ class CSVReader
         static std::vector<OrderBookEntry>readCSV(std::string csvFile);
     private:
         static std::vector<std::string> tokenise(std::string csvLine, char seperator);
-        static OrderBookEntry stringsToOBE(std::vector<std::string> strings);
+        static OrderBookEntry stringsToOBE(std::vector<std::string> strings)
 
 
 };
